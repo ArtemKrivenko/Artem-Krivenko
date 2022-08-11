b64f9b0146f2c324a51c872fb8279e2165e09c0e
ArtemKrivenko <110335994+ArtemKrivenko@users.noreply.github.com>
2022-08-11 17:35:59 +0300

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,16 @@
+{public abstract class Human
+    {public override string ToString()
+    {int.Age = DateTime.Now.Year - this.Age.Year;
+        return string.Format("Name: {0}\nSurname: {1}\nAge: {2}\n", this.Name, this.Age);}
+    public void Create(string Name, Age)
+    }}
+class Program {
+        static void Main(string[] args) {
+        Human = new Human-Human();
+        human.Create ("Artem", "Krivenko", (2002, 08, 14));
+        Console.WriteLine (Human.ToString());
+        Console.ReadKey();
+        }
+}
+}
+/*Не очень понял тему, пересмотрю урок*/
---
+++ 2.js
@@ -0,0 +1,5 @@
+public class Program{
+
+    public static void main(String[] args) {
+    Frontend dev = new Frontdev("Developer", "Work");dev.display();
+    Builder = new Builder("Builder", "Work");builder.display();}}
